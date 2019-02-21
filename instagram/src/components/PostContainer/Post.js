bea136db0b3e14ab2b06b041f3ca52613152c67c
Tabita Filimon <tabitafilimon@gmail.com>
2019-02-21 18:01:14 -0500

    refactored posts

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import PropTypes from 'prop-types';
-import './Post.css';
+import styled from 'styled-components';
+
+const PostDiv = styled.div`
+border: 1px solid lightgray;
+width: 60%;
+margin: 80px 0 40px 40px;
+`;
+
+const UserDiv = styled.div`
+display: flex;
+align-items:center;
+padding: 10px 0;
+`;
+
+const UserImg = styled.img`
+width: 35px;
+height: auto;
+border-radius: 50%;
+margin-left: 10px;
+`;
+
+const UserName = styled.h4`
+padding: 0 10px;
+`;
+
+const PostImg = styled.img`
+width: 100%;
+height: auto;
+`;
 
 const Post = props => {
     return(
-        <div className= "post">
-            <div className= "user">
-                <img className="user-image" src={props.post.thumbnailUrl} alt={'username thumbnail'} />
-                <h4 className= "user-name">{props.post.username}</h4>
-            </div>
-            <img className="post-image" src={props.post.imageUrl} alt={'post image'}/>
+        <PostDiv>
+            <UserDiv>
+                <UserImg src={props.post.thumbnailUrl} alt={'username thumbnail'} />
+                <UserName>{props.post.username}</UserName>
+            </UserDiv>
+            <PostImg src={props.post.imageUrl} alt={'post image'}/>
             <CommentSection comments= {props.post.comments}  data={props.post} likes={props.post.likes}/>                   
-        </div>
+        </PostDiv>
     );
 }
 
